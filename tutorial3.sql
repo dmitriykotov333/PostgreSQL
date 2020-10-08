dbe35c7ab4c7243ebcdeef4110bed4d6c9b70c53
Dmitriy Kotov <40573791+dmitriykotov333@users.noreply.github.com>
2020-10-08 18:09:41 +0300

    tutorial3
    not null, default

---
+++ tutorial3.sql
@@ -0,0 +1,15 @@
+-- создание таблицы
+--create table my_contact (
+--	last_name varchar(30) not null default 'last_name',
+--	first_name varchar(20) not null default 'first_name',
+--	email varchar(50) not null default 'email',
+--	gender varchar(1) not null default 'g',
+--	birthday date not null default '1111-11-11',
+--	profession varchar(50),
+--	location varchar(50) not null default 'location',
+--	status varchar(20) not null default 'status',
+--	interest varchar(100) not null default 'interest',
+--	seeking varchar(100) not null default 'seeking'
+--)
+insert into my_contact values('test');
+select * from my_contact
